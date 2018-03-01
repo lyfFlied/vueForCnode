5fbad93ce006b32ddf9118eb6ce8e7ff166069ff
modan <1072110269@qq.com>
2018-03-01 22:20:14 +0800

    清理多余文件，代码

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_URL: '"https://api.laravue.org/api/v1"' // 获取外部数据的接口
+  NODE_ENV: '"development"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,4 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"',
-  API_URL: '"https://api.laravue.org/api/v1"' // 获取外部数据的接口
+  NODE_ENV: '"production"'
 }
