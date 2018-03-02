160c68f2a7ce6d3f0badc77d94d1f6377cf50b2d
modan <1072110269@qq.com>
2018-03-03 00:58:54 +0800

    element-ui 按需加载

--- src/tools/api.js
+++ src/tools/api.js
@@ -11,7 +11,7 @@ export class LoginApi {
     return server.connection('POST', '/api/sessions', params)
   }
 
-  static logout  () {
+  static logout   () {
     return server.connection('POST', '/api/logout')
   }
 }
